ad6de3dd854ca0b3619ae2281ee24d76c78db5ec
Kang1221 <90603399+Kang1221@users.noreply.github.com>
2022-06-02 02:27:40 +0900

    Table 보기 기능 추가 & 건물정보 수정 코드 추가

--- src/INSERT_OWNER.java
+++ src/INSERT_OWNER.java
@@ -1,13 +1,32 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Vector;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
 import java.awt.*;
+import javax.swing.event.TableModelEvent;
+
 
 public class INSERT_OWNER extends JFrame implements ActionListener{
 	JTextField txt1, txt2, txt3;
+	public Statement s;
+	public ResultSet r;
+	
+	private Vector<String> Head = new Vector<String>();
+	
+	private JTable table;
+	private DefaultTableModel model;
+	private int ID_COLUMN = 0;
+	private int NAME_COLUMN = 0;
+	private int NUMBER_COLUMN = 0;
+
+	private RoundedButton Show_Button = new RoundedButton("보기");
+	private RoundedButton Insert_Button = new RoundedButton("등록");
+	Container me = this;
+	
+	JPanel panel;
+	JScrollPane ScPane;
+	int count = 0;
 	
 	public INSERT_OWNER ( ){
 		setTitle("어쩔매물 DB Insert Owner Service");
@@ -16,100 +35,185 @@ public class INSERT_OWNER extends JFrame implements ActionListener{
 		setSize(700, 700);
 		getContentPane().setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 		Font f1 = new Font("아임크리수진",Font.PLAIN, 13);
 		
-		// 레이블 생성
-		JLabel lbl1 = new JLabel("id ");
-		lbl1.setBounds(220, 50, 200, 20);
-		lbl1.setFont(f1);
 		
+		// 레이블 생성
+		JLabel lbl1 = new JLabel("집주인 id ");
+		lbl1.setFont(f1);
 		JLabel lbl2 = new JLabel("집주인 이름  ");
-		lbl2.setBounds(220, 120, 200, 20); 
 		lbl2.setFont(f1);
 		JLabel lbl3 = new JLabel("집주인 전화번호  ");
-		lbl3.setBounds(220, 190, 200, 20);
 		lbl3.setFont(f1);
 		
-		// 20자리 텍스트 필드 생성
-		txt1 = new JTextField(5); 
-		txt1.setBounds(220, 80, 100, 20);
-
+		//텍스트 필드 생성
+		txt1 = new JTextField("Oid000", 6); 
 		txt2 = new JTextField(20); 
-		txt2.setBounds(220, 150, 100, 20);
-
 		txt3 = new JTextField(20); 
-		txt3.setBounds(220, 220, 200, 20);
 
+		
+		JPanel ShowAllPanel = new JPanel();
+		ShowAllPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		ShowAllPanel.add(Show_Button);
+		ShowAllPanel.add(Insert_Button);
+		
+		JPanel UpdatePanel = new JPanel();
+		UpdatePanel.setLayout(new GridLayout(3,3));
+		UpdatePanel.add(lbl1);				UpdatePanel.add(txt1);
+		UpdatePanel.add(lbl2);		UpdatePanel.add(txt2);
+		UpdatePanel.add(lbl3);
 
-		getContentPane().add(lbl1);		getContentPane().add(txt1);
-		getContentPane().add(lbl2);		getContentPane().add(txt2);
-		getContentPane().add(lbl3);		getContentPane().add(txt3);
 		
+		UpdatePanel.add(txt3);
 		
-		RoundedButton btn = new RoundedButton("등록");
-		btn.setBounds(350, 270, 70, 30);
-		btn.setFont(f1);
-		getContentPane().add(btn);
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new FlowLayout());
+		contentPane.setBackground(new Color(244, 244, 244));
+			
+		contentPane.add(UpdatePanel);
+		contentPane.add(ShowAllPanel);
 		
-		btn.addActionListener(this);
-		
+		Show_Button.addActionListener(this);
+		Insert_Button.addActionListener(this);
+
 		setVisible(true);
 	}
-	public static void main(String args[]){
-		new INSERT_OWNER();
-    }
+	
+	
 	 @Override
-		public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e) {
 	        Connection conn = null;
-	        String id="";
+	        
+	        //JDBC 연결 부분
 	        try{
-	            String url = "jdbc:mysql://localhost:3306/db2022team11";
+	            String url = "jdbc:mysql://localhost:3306/DB2022Team11";
 	       	 
 	        	//Database user, password
-	        	String  user = "DBTeam11";
-	        	String password ="DBTeam11";
-
+	        	String  user = "DB2022Team11";
+	        	String password ="DB2022Team11";
+	        	
 	            conn = DriverManager.getConnection(url, user, password);
 	            System.out.println("Insert_OWNER Successfully Connection!");	//연결 확인 메세지
 	            conn.setAutoCommit(false);
 
-	            String id_num = txt1.getText();
-	            String name = txt2.getText();
-	            String number = txt3.getText();
-			
-	            if(id_num.length() <= 1) { id = "Oid00" + id_num; }
-	            else if(id_num.length() == 2) {	id = "Oid0" + id_num; }       
-	            else if(id_num.length() == 3) {id = "Oid" + id_num;}         	
-	            else  {JOptionPane.showMessageDialog(null, "id는 세자리까지 입력가능합니다.", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);}
-	            
-	            PreparedStatement pStmt = conn.prepareStatement(
-	            		"insert into DB2022_OWNER values(?,?,?)");
-	            
-	          	pStmt.setString(2, name);
-	           	pStmt.setString(3, number);
-	           	
-	           	//id는 not null
-	           	if(id_num.isEmpty()) {                  
-	            	JOptionPane.showMessageDialog(null, "집주인 id번호를 입력해주세요. \n", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE); 
-	            }   
-	           	else {
-	           		pStmt.setString(1, id);               
-	           	}
-	           	pStmt.executeUpdate(); 
-	           	conn.commit();
-	        	JOptionPane.showMessageDialog(null, "입력하신 집주인을 등록하였습니다.");
 	        }
-	        catch (SQLException sqle) {
-	        	sqle.printStackTrace();
+	        catch(SQLException e1) {
+				System.out.println("연결할 수 없습니다.");
+				e1.printStackTrace();
+			}
+	        
+	        
+			// --------- // 
+			if(count==1) {
+				me.remove(panel);
+				revalidate();
+			}
+			
+			
+			 // ----------- OWNER TABLE 보기 ----------- //
+			if(e.getSource() == Show_Button) {
+				Head.clear();
+				Head.add("owner_id");
+				Head.add("owner_name");
+				Head.add("owner_number");
+				
+				String stmt = "SELECT * FROM DB2022_OWNER";
+				
+				model = new DefaultTableModel(Head, 0) {
+					@Override
+					public boolean isCellEditable(int row, int column) {
+						if(column > 0) {
+							return false;
+						} else {
+							return true;
+						}
+					}
+				};
+				
+				table = new JTable(model);
+				
 				try {
-					if(conn!=null)
-						conn.rollback();
-				}catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-	        	System.out.println("SQLException : " + sqle);
-	        	JOptionPane.showMessageDialog(null, "새로운 집주인 등록에 실패했습니다. \n", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);  
-	        } 
-		}
+					count = 1;
+					s = conn.createStatement();
+					r = s.executeQuery(stmt);
 
+					while(r.next()) {
+						String[] input = new String[4];
+						input[0] = r.getString("owner_id");
+						input[1] = r.getString("owner_name");
+						input[2] = r.getString("owner_number");
+						model.addRow(input);
+					}
+					
+				} catch(SQLException ee) {
+					System.out.println("actionPerformed err: " + ee);
+					ee.printStackTrace();
+				}
+				
+				panel = new JPanel();
+				ScPane = new JScrollPane(table);
+				ScPane.setPreferredSize(new Dimension(600, 500));
+				panel.add(ScPane);
+				add(panel,BorderLayout.CENTER);
+				revalidate();
+			}
+	        
+			
+	        
+			// ------------ INSERT 집주인 ---------------//
+	        if(e.getSource() == Insert_Button) {
+	        	try {
+	        		String id_num = txt1.getText();
+		            String name = txt2.getText();
+		            String number = txt3.getText();
+				 
+		            PreparedStatement pStmt = conn.prepareStatement(
+		            		"insert into DB2022_OWNER values(?,?,?)");
+		            
+		            //ID 네자리 이상 불가능
+		            if(id_num.length() > 6) {JOptionPane.showMessageDialog(null, "id는 세자리까지 입력가능합니다.", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);}	            
+		           	//ID null값 불가능
+		           	if(id_num.equals("Oid000") || id_num.isEmpty()) {                  
+		            	JOptionPane.showMessageDialog(null, "집주인 id번호를 입력해주세요. \n", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE); 
+		            }   
+		           	else {pStmt.setString(1, id_num);}
+		          	pStmt.setString(2, name);
+		           	pStmt.setString(3, number);
+		           	pStmt.executeUpdate(); 
+		           	conn.commit();
+		        	JOptionPane.showMessageDialog(null, "입력하신 집주인을 등록하였습니다.");
+	        	}
+		        catch (SQLException sqle) {
+		        	sqle.printStackTrace();
+					try {
+						if(conn!=null)
+							conn.rollback();
+					}catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+		        	System.out.println("SQLException : " + sqle);
+		        	JOptionPane.showMessageDialog(null, "새로운 집주인 등록에 실패했습니다. \n", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);  
+		        }
+            	
+            }
+            
+        
+
+        } 
+	 	public void tableChanged(TableModelEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	 	public static void main(String args[]){
+			new INSERT_OWNER();
+	    }
+		
 }
+	 
+	 
+	 
+	
+
+
--- src/INSERT_SALE.java
+++ src/INSERT_SALE.java
@@ -1,9 +1,14 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.Vector;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 import org.w3c.dom.ls.LSOutput;
 
@@ -15,194 +20,274 @@ public class INSERT_SALE extends JFrame implements ActionListener{
 	JTextField txt1, txt2, txt3, txt4, txt6, txt7,txt8,txt9;
 	JRadioButton rd1, rd2, rd3;
 	
+	public Statement s;
+	public ResultSet r;
+	
+	private Vector<String> Head = new Vector<String>();
+	
+	private JTable table;
+	private DefaultTableModel model;
+	private int ID_COLUMN = 0;
+	private int NAME_COLUMN = 0;
+	private int NUMBER_COLUMN = 0;
+
+	private RoundedButton Show_Button = new RoundedButton("보기");
+	private RoundedButton Insert_Button = new RoundedButton("등록");
+	Container me = this;
+	
+	JPanel panel;
+	JScrollPane ScPane;
+	int count = 0;
+	
 	public INSERT_SALE ( ){
 		setTitle("어쩔매물 DB Insert Sale Service");
 		Color b=new Color(244,244,244);
 		setBackground(b);
 		setSize(700, 700);
-		getContentPane().setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 		Font f1 = new Font("아임크리수진",Font.PLAIN, 13);
 		
 		// 레이블 생성
 		JLabel lbl1 = new JLabel("매물 id ");
-		lbl1.setBounds(220, 50, 200, 20);
 		lbl1.setFont(f1);
-		
 		JLabel lbl2 = new JLabel("부동산 id  ");
-		lbl2.setBounds(220, 100, 200, 20); 
 		lbl2.setFont(f1);
-		
 		JLabel lbl3 = new JLabel("집주인 id ");
-		lbl3.setBounds(220, 150, 200, 20);
 		lbl3.setFont(f1);
-		
 		JLabel lbl4 = new JLabel("지역 id ");
-		lbl4.setBounds(220, 200, 200, 20);
 		lbl4.setFont(f1);
-		
 		JLabel lbl5 = new JLabel("매물 종류 ");
-		lbl5.setBounds(220, 250, 200, 20);
 		lbl5.setFont(f1);
-		
-		JLabel lbl6 = new JLabel("매매가/전세가                           보증금 ");
-		lbl6.setBounds(220, 300, 400, 20);
+		JLabel lbl6 = new JLabel("매매가/전세가 ");
 		lbl6.setFont(f1);
-		
+		JLabel lbl7 = new JLabel(" 보증금 ");
+		lbl7.setFont(f1);
 		JLabel lbl8 = new JLabel("건물 id ");
-		lbl8.setBounds(220, 350, 200, 20);
 		lbl8.setFont(f1);
 		
 		JLabel lbl9 = new JLabel("상세 주소");
-		lbl9.setBounds(220, 400, 200, 20);
 		lbl9.setFont(f1);
 		
 		JLabel won1 = new JLabel("만원");
-		won1.setBounds(340, 320, 120, 20);
 		won1.setFont(f1);
 		JLabel won2 = new JLabel("만원");
-		won2.setBounds(520, 320, 120, 20);
 		won2.setFont(f1);
 		
 		
 		//텍스트 필드 생성
-		txt1 = new JTextField(5); 
-		txt1.setBounds(220, 70, 100, 20);
-
-		txt2 = new JTextField("Aid000", 20); 
-		txt2.setBounds(220, 120, 100, 20);
-		
-		txt3 = new JTextField("Oid000", 20); 
-		txt3.setBounds(220, 170, 100, 20);
-		
-		txt4 = new JTextField("Lid000", 20); 
-		txt4.setBounds(220, 220, 100, 20);
-		
+		txt1 = new JTextField("Pid000",6); 
+		txt2 = new JTextField("Aid000", 6); 
+		txt3 = new JTextField("Oid000", 6); 
+		txt4 = new JTextField("Lid000", 6); 
 		txt6 = new JTextField(20); //매매가
-		txt6.setBounds(220, 320, 120, 20);
-		
-		txt7 = new JTextField(20); //보증금
-		txt7.setBounds(400, 320, 120, 20);
-		
-		txt8 = new JTextField("Bid000", 20); //빌딩아이디
-		
-		txt8.setBounds(220, 370, 100, 20);
+		txt7 = new JTextField(20); //보증금	
+		txt8 = new JTextField("Bid000", 20); //빌딩아이디	
 		txt9 = new JTextField(20); //상세주소
-		txt9.setBounds(220, 420, 300, 50);
-
-		getContentPane().add(lbl1);		getContentPane().add(txt1);
-		getContentPane().add(lbl2);		getContentPane().add(txt2);
-		getContentPane().add(lbl3);		getContentPane().add(txt3);
-		getContentPane().add(lbl4);		getContentPane().add(txt4);
-		getContentPane().add(lbl5);		getContentPane().add(lbl6);		
-		getContentPane().add(txt6);  	getContentPane().add(won1);
-		getContentPane().add(txt7); 	getContentPane().add(won2);
-		getContentPane().add(lbl8);		getContentPane().add(txt8);
-		getContentPane().add(lbl9);		getContentPane().add(txt9);
-		
+	
 		rd1 = new JRadioButton("매매"); 	rd1.setBounds(220, 270, 50, 20);
         rd2 = new JRadioButton("전세");	rd2.setBounds(300, 270, 50, 20);
         rd3 = new JRadioButton("월세");	rd3.setBounds(380, 270, 50, 20);
-
         // 라디오 버튼을 그룹화 하기위한 객체 생성
         ButtonGroup groupRd = new ButtonGroup();
         groupRd.add(rd1);
         groupRd.add(rd2);
         groupRd.add(rd3);
         
-        getContentPane().add(rd1);
-        getContentPane().add(rd2);
-        getContentPane().add(rd3);
 
-        
-        
-		RoundedButton btn = new RoundedButton("등록");
-		btn.setBounds(400, 600, 70, 30);
-		btn.setFont(f1);
-		getContentPane().add(btn);
+        JPanel ShowAllPanel = new JPanel();
+		ShowAllPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		ShowAllPanel.add(Show_Button);
+		ShowAllPanel.add(Insert_Button);
 		
-		btn.addActionListener(this);
+		JPanel UpdatePanel = new JPanel();
+		UpdatePanel.setLayout(new GridLayout(10,4));
+		UpdatePanel.add(lbl1);			UpdatePanel.add(txt1);
+		UpdatePanel.add(lbl2);			UpdatePanel.add(txt2);
+		UpdatePanel.add(lbl3);			UpdatePanel.add(txt3);
+		UpdatePanel.add(lbl4);			UpdatePanel.add(txt4);
+		UpdatePanel.add(lbl5);			UpdatePanel.add(rd1);	UpdatePanel.add(rd2);	UpdatePanel.add(rd3);
+		UpdatePanel.add(lbl6);			UpdatePanel.add(txt6); 
+		UpdatePanel.add(lbl7);			UpdatePanel.add(txt7);
+		UpdatePanel.add(lbl8);			UpdatePanel.add(txt8);
+		UpdatePanel.add(lbl9);			UpdatePanel.add(txt9);
+
+
 		
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new FlowLayout());
+		contentPane.setBackground(new Color(244, 244, 244));
+			
+		contentPane.add(UpdatePanel);
+		contentPane.add(ShowAllPanel);
+		
+		Show_Button.addActionListener(this);
+		Insert_Button.addActionListener(this);
+
 		setVisible(true);
-	}
+	} 
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		 Connection conn = null;
+	        
+	     //JDBC 연결 부분
+	     try{
+	    	 String url = "jdbc:mysql://localhost:3306/DB2022Team11";
+	       	 
+	        //Database user, password
+	        String  user = "DB2022Team11";
+	        String password ="DB2022Team11";
+	        	
+	        conn = DriverManager.getConnection(url, user, password);
+	        System.out.println("Insert_AGENCY Successfully Connection!");	//연결 확인 메세지
+	        conn.setAutoCommit(false);
+	     }
+	     catch(SQLException e1) {
+	    	System.out.println("연결할 수 없습니다.");
+			e1.printStackTrace();
+	     }
+	     
+			// --------- // 
+			if(count==1) {
+				me.remove(panel);
+				revalidate();
+			}
+			
+			
+			 // ----------- SALE TABLE 보기 ----------- //
+			if(e.getSource() == Show_Button) {
+				Head.clear();
+				Head.add("sale id");
+				Head.add("agency id");
+				Head.add("owner id");
+				Head.add("area id");
+				Head.add("rent type");
+				Head.add("price");
+				Head.add("deposit");
+				Head.add("sale date");
+				Head.add("building id");
+				Head.add("address");
+				
+				String stmt = "SELECT * FROM DB2022_SALE";
+				
+				model = new DefaultTableModel(Head, 0) {
+					@Override
+					public boolean isCellEditable(int row, int column) {
+						if(column > 0) {
+							return false;
+						} else {
+							return true;
+						}
+					}
+				};
+
+				
+				table = new JTable(model);
+				
+				try {
+					count = 1;
+					s = conn.createStatement();
+					r = s.executeQuery(stmt);
+
+					while(r.next()) {
+						String[] input = new String[10];
+						input[0] = r.getString("Pid");
+						input[1] = r.getString("agency_id");
+						input[2] = r.getString("owner_id");
+						input[3] = r.getString("area_id");
+						input[4] = r.getString("rent_type");
+						input[5] = r.getString("price");
+						input[6] = r.getString("deposit");
+						input[7] = r.getString("sale_date");
+						input[8] = r.getString("building_id");
+						input[9] = r.getString("address");
+						
+						model.addRow(input);
+					}
+					
+				} catch(SQLException ee) {
+					System.out.println("actionPerformed err: " + ee);
+					ee.printStackTrace();
+				}
+				
+				panel = new JPanel();
+				ScPane = new JScrollPane(table);
+				ScPane.setPreferredSize(new Dimension(650, 350));
+				panel.add(ScPane);
+				add(panel,BorderLayout.CENTER);
+				revalidate();
+			}
+			
+			
+			
+			// ------------ INSERT 매물 ---------------//
+			if(e.getSource() == Insert_Button) {
+				try{
+					String id_num, agency_id, owner_id, area_id, rent_type, date, building_id , address, price, deposit;
+					id_num = txt1.getText();					
+		            agency_id = txt2.getText(); 				owner_id = txt3.getText();   		   
+		            area_id = txt4.getText();					rent_type = null;	
+		            price = txt6.getText();	
+		            deposit = txt7.getText();				
+		            building_id = txt8.getText();			address = txt9.getText();
+		            
+		            // 현재 날짜 구하기
+		            LocalDate now = LocalDate.now();
+		            date = now.toString();
+	
+		            if(rd1.isSelected())
+		            	rent_type = rd1.getText();
+		            else if(rd2.isSelected())
+		            	rent_type = rd2.getText();
+		            else if(rd3.isSelected())
+		            	rent_type = rd3.getText();
+		            else 
+		            	JOptionPane.showMessageDialog(null, "건물 종류를 선택해주세요 \n", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);
+		            
+		            PreparedStatement pStmt = conn.prepareStatement(
+		            		"insert into DB2022_SALE values(?,?,?,?,?,?,?,?,?,?)");
+		            
+		            //ID 네자리 이상 불가능
+		            if(id_num.length() > 6) {JOptionPane.showMessageDialog(null, "id는 세자리까지 입력가능합니다.", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);}	            
+		           	//ID null값 불가능
+		           	if(id_num=="Pid000" || id_num.isEmpty()) {//id는 not null                  
+		            	JOptionPane.showMessageDialog(null, "매뭏 id번호를 입력해주세요. \n", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE); 
+		            }   
+		           	else { pStmt.setString(1, id_num);}
+		          	pStmt.setString(2, agency_id);
+		           	pStmt.setString(3, owner_id);
+		          	pStmt.setString(4, area_id);
+		           	pStmt.setString(5, rent_type);
+		           	if(!price.isBlank())   {pStmt.setInt(6, Integer.parseInt(price));}
+		           	else {pStmt.setString(6,null);}
+		           	if(!deposit.isBlank()) {pStmt.setInt(7, Integer.parseInt(deposit));}
+		           	else {pStmt.setString(7,null);}
+		           	pStmt.setString(8, date);
+		           	pStmt.setString(9, building_id);
+		           	pStmt.setString(10, address);
+		           	pStmt.executeUpdate();
+		           	conn.commit();
+		        	JOptionPane.showMessageDialog(null, "입력하신 매물을 등록하였습니다.");
+		        }catch (SQLException sqle) {
+		        	sqle.printStackTrace();
+					try {
+						if(conn!=null)
+							conn.rollback();
+					}catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+		        	System.out.println("SQLException : " + sqle);
+		        	JOptionPane.showMessageDialog(null, "새로운 매물 등록에 실패했습니다. \n", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE); 
+		        } 
+			}
+		}
+	
+	
+	
 	public static void main(String args[]){
 		new INSERT_SALE();
    }
 	
-	 @Override
-		public void actionPerformed(ActionEvent e) {
-	        Connection conn = null;
-	        String id = "";
-	        try{
-	            String url = "jdbc:mysql://localhost:3306/db2022team11";
-	 
-	        	//Database user, password
-	        	String  user = "DBTeam11";
-	        	String password ="DBTeam11";
 
-	            conn = DriverManager.getConnection(url, user, password);
-	            System.out.println("Insert_SALE Successfully Connection!");	//연결 확인 메세지
-	            conn.setAutoCommit(false);
-
-	            String id_num, agency_id, owner_id, area_id, rent_type, date, building_id , address, price, deposit;
-	            id_num = txt1.getText();					
-	            agency_id = txt2.getText(); 				owner_id = txt3.getText();   		   
-	            area_id = txt4.getText();					rent_type = null;	
-	            price = txt6.getText();	
-	            deposit = txt7.getText();				
-	            building_id = txt8.getText();			address = txt9.getText();
-	            
-	            // 현재 날짜 구하기
-	            LocalDate now = LocalDate.now();
-	            date = now.toString();
-	            
-	            if(id_num.length() <= 1) { id = "Pid00" + id_num; }
-	            else if(id_num.length() == 2) {	id = "Pid0" + id_num; }       
-	            else if(id_num.length() == 3) {id = "Pid" + id_num;}         	
-	            else  {JOptionPane.showMessageDialog(null, "id는 세자리까지 입력가능합니다.", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);}
-
-	            if(rd1.isSelected())
-	            	rent_type = rd1.getText();
-	            else if(rd2.isSelected())
-	            	rent_type = rd2.getText();
-	            else if(rd3.isSelected())
-	            	rent_type = rd3.getText();
-	            else 
-	            	JOptionPane.showMessageDialog(null, "건물 종류를 선택해주세요 \n", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);
-	            
-	            PreparedStatement pStmt = conn.prepareStatement(
-	            		"insert into DB2022_SALE values(?,?,?,?,?,?,?,?,?,?)");
-	            
-	           	if(id_num.isEmpty()) {//id는 not null                  
-	            	JOptionPane.showMessageDialog(null, "건물 id번호를 입력해주세요. \n", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE); 
-	            }   
-	           	else { pStmt.setString(1, id);}
-	          	pStmt.setString(2, agency_id);
-	           	pStmt.setString(3, owner_id);
-	          	pStmt.setString(4, area_id);
-	           	pStmt.setString(5, rent_type);
-	           	if(!price.isBlank())   {pStmt.setInt(6, Integer.parseInt(price));}
-	           	else {pStmt.setString(6,null);}
-	           	if(!deposit.isBlank()) {pStmt.setInt(7, Integer.parseInt(deposit));}
-	           	else {pStmt.setString(7,null);}
-	           	pStmt.setString(8, date);
-	           	pStmt.setString(9, building_id);
-	           	pStmt.setString(10, address);
-	           	pStmt.executeUpdate();
-	           	conn.commit();
-	        	JOptionPane.showMessageDialog(null, "입력하신 매물을 등록하였습니다.");
-	        }
-	        catch (SQLException sqle) {
-	        	sqle.printStackTrace();
-				try {
-					if(conn!=null)
-						conn.rollback();
-				}catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-	        	System.out.println("SQLException : " + sqle);
-	        	JOptionPane.showMessageDialog(null, "새로운 매물 등록에 실패했습니다. \n", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE); 
-	        }
-	
- }
 }
